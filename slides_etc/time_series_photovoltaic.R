70889a618958aca9e0ce8a712f38f8b6cb26f453
Cory Whitney <whitney.cory@gmail.com>
2021-06-09 23:33:14 +0200

    Add materials for seminar and lecture 8

--- slides_etc/time_series_photovoltaic.R
+++ slides_etc/time_series_photovoltaic.R
@@ -7,35 +7,30 @@ var_CV<-20
 initial_investment<-1000
 price<-3
 
-# value-varier
+decision_model <- function(){
 
-decision_model<-function(x, var)
-{
+yields <- vv(var_mean = yield, 
+             var_CV = var_CV, 20)
 
-yields<-vv(var_mean=yield,var_CV=var_CV,20)
-prices<-vv(var_mean=price,var_CV=var_CV,20)
-p_hail<-0.2
+prices <- vv(var_mean = price, 
+             var_CV = var_CV, 20)
 
-costs<-c(initial_investment,rep(0,19))
+p_hail <- 0.2
 
+# calculate costs for 20 years with 'rep'
+costs <- c(initial_investment, rep(0, 19))
 
+# use 'chance_event' to adjust yield for potential hail
+hail_adjusted_yield <- chance_event(p_hail, 
+                                    value_if = 0,
+                                    value_if_not = yield,
+                                    n = 20,
+                                    CV_if = var_CV)
 
+# calculate profit
+profit <- (hail_adjusted_yield*prices)-costs
 
-plot(gompertz_yield(max_harvest=1000,
-               time_to_first_yield_estimate=5,
-               time_to_second_yield_estimate=15,
-               first_yield_estimate_percent=10,
-               second_yield_estimate_percent=90,
-               n_years=30,
-               var_CV=0,
-               no_yield_before_first_estimate=FALSE))
+# use 'discount' to calculate net present value
+NPV <- discount(profit, 5, calculate_NPV = TRUE)
 
-
-yield_hail_adjusted<-chance_event(p_hail,value_if=0,value_if_not=yield,n=20,CV_if=var_CV)
-
-profit<-yield_hail_adjusted*prices-costs
-#sum_cashflow<-sum(profit)
-
-
-return(discount(profit,5,calculate_NPV=TRUE))
-}
+return(NPV = NPV) }
